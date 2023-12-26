23396c00529651190055dd76b18021a43d749cb8
DominMFD <mfdmatheus@hotmail.com>
2023-12-26 12:46:23 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("329995549", "Iphone 15", "12435", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+Nokia nokia = new Nokia ("32997897", "Nokia 1100", "13235", 12);
+nokia.Ligar();
+nokia.InstalarAplicativo("FreeFire");
